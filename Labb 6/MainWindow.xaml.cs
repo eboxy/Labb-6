c73e52e0bd9362fc2bda41d1c1b028eb59975990
eboxy55 <eboxy55@gmail.com>
2017-10-30 20:32:14 +0100

    Add project files.

---
+++ Labb 6/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb_6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        
+
+    }  //mainwindow class ends here
+    
+    // OTHER CLASSES HERE:
+
+    public class Bouncer
+    {
+        
+    }
+
+    public class Patron
+    {
+        
+    }
+
+
+    public class Bartender
+    {
+        
+
+    }
+
+    public class Waiter
+    {
+        
+    }
+
+
+
+
+
+} //EOF
