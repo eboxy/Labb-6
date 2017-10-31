c32fb0fada96577033478b5a542fd889922490c4
eboxy55 <eboxy55@gmail.com>
2017-10-31 15:39:48 +0100

    fixa delegate med medskickat värde

--- Labb 6/MainWindow.xaml.cs
+++ Labb 6/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,43 @@ namespace Labb_6
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Bartender b = new Bartender();
+
+            //Action < T >  ??????
+
+
+            int t = 0;
+
+            
+            
+
+            //Action<int> action = new Action<int>(PrintGuest);
+            //action("Hello!!!");
+
+            lstbBartender.Items.Add("´Puben frågar bartender");
+            //Task taskToBartender = new Task(new Action<int>(PrintGuest(t));  //   VARFÖR FUNKAR NEDANSTÅENDE TASK OCH INTE DENNA??
+
+            Task taskToBartender = new Task(() => PrintGuest(t));
+            b.InviteGuest(taskToBartender);
+
+            
+        }  //mainwindow-method ends here!!!
+
+        public void PrintGuest(int numOfGuests)
+        {
+
+            Dispatcher.Invoke(() =>
+            {
+                lstbBartender.Items.Add("Svar från bartender " + numOfGuests);
+            });
+           
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -38,22 +73,35 @@ namespace Labb_6
     public class Bouncer
     {
         
-    }
+        
+   }
 
     public class Patron
     {
-        
+        //utskrifts-callback till motsvarande listbox
+
+
+
     }
 
 
     public class Bartender
     {
-        
+        //utskrifts-callback till motsvarande listbox
+
+        public void InviteGuest(Task fromPub)
+        {
+            //Task t = fromPub;
+           fromPub.Start();
+           
+        }
 
     }
 
     public class Waiter
     {
+        //utskrifts-callback till motsvarande listbox
+
         
     }
 
