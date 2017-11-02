b4d57879c8106a5fbe21310e3dae03a938e63bb5
eboxy55 <eboxy55@gmail.com>
2017-11-02 15:58:03 +0100

    fått till några fler utskrifter m.m

--- Labb 6/MainWindow.xaml.cs
+++ Labb 6/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Labb_6
 {
@@ -29,28 +30,40 @@ namespace Labb_6
             InitializeComponent();
 
             Bartender b = new Bartender();
-            int t = 0;
 
-            lstbBartender.Items.Add("´Puben frågar bartender");
+            b.PourBeer += WaitingForGuest;
+            b.PourBeer += OnBeer;
+            b.PourBeer += GastDrinksBeer;
+            b.PouringBeer();
+
+
+
+            //int t = 0;
+
+            //lstbBartender.Items.Add("´Puben frågar bartender");
             //Task taskToBartender = new Task(new Action<int>(PrintGuest(t));  //   VARFÖR FUNKAR NEDANSTÅENDE TASK OCH INTE DENNA??
 
-            Task taskToBartender = new Task(() => PrintGuest(t));
-            b.InviteGuest(taskToBartender);
+            //Task taskToBartender = new Task(() => PrintGuest(t));
+            //b.InviteGuest(taskToBartender);
 
-            
+
+
+
+
+
         }  //mainwindow-method ends here!!!
 
-        public void PrintGuest(int numOfGuests)
-        {
+        //public void PrintGuest(int numOfGuests)
+        //{ Dispatcher.Invoke(() => { lstbBartender.Items.Add("Svar från bartender " + numOfGuests);});}
 
-            Dispatcher.Invoke(() =>
-            {
-                lstbBartender.Items.Add("Svar från bartender " + numOfGuests);
-            });
-           
-        }
+        public void GastDrinksBeer()
+        { Dispatcher.Invoke(() => { lstbGaster.Items.Add(3 + "_Gästen dricker upp ölen"); });}
 
-        
+        public void OnBeer()
+        { Dispatcher.Invoke(() => { lstbBartender.Items.Add(2 + "_Har hällt bärs här hela dan "); }); }
+
+        public void WaitingForGuest()
+        { Dispatcher.Invoke(() => { lstbBartender.Items.Add(1 + "_Väntar på gästen "); }); }
 
     }  //mainwindow class ends here
     
@@ -75,6 +88,26 @@ namespace Labb_6
     {
         //utskrifts-callback till motsvarande listbox
 
+        //public event Action Run, Roar; 
+
+        public event Action PourBeer;
+
+
+        public void PouringBeer()
+        {
+
+            Task.Run(() =>
+            {
+               PourBeer?.Invoke();
+
+            });
+
+            //ingen task, metod som anropar häll upp öl osv. task i main dock!!
+
+        }
+
+        
+        
         public void InviteGuest(Task fromPub)
         {
           fromPub.Start();
