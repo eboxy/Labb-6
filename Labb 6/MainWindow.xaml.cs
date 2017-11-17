66736e454f9498eccb4eb0626e43d011b4411208
eboxy55 <eboxy55@gmail.com>
2017-11-17 21:48:26 +0100

    bla fixat sleep-loopar :)

--- Labb 6/MainWindow.xaml.cs
+++ Labb 6/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Windows.Media.Imaging;                                         
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -23,6 +23,8 @@ namespace Labb_6
 {
     public partial class MainWindow : Window
     {
+        
+
         public static String GetTimestamp(DateTime value)
         {
             return value.ToString("[HH:mm:ss]");
@@ -52,13 +54,15 @@ namespace Labb_6
 
         //Queues
         ConcurrentStack<Glass> glasstack = new ConcurrentStack<Glass>();
+
         ConcurrentStack<Chair> chairStack = new ConcurrentStack<Chair>();
         ConcurrentQueue<Guest> guestQueue = new ConcurrentQueue<Guest>();
 
         //Create Glasstack
-        private void CreateAGlass()
+        private void CreateAGlass()   //borde vara i bartenderklassen samt borde heta ngt i stil med CreateGlasses, det är ju flera!!
         {
-            Task.Run(() => {
+            Task.Run(() =>
+            {
                 for (int i = 0; i < 8; i++)
                 {
                     glasstack.Push(new Glass());
@@ -66,9 +70,10 @@ namespace Labb_6
             });
         }
 
-        private void CreateAChair()
+        private void CreateAChair()   //borde vara i waiter-klassen + heta CreateCharis, också flera vettö!!!
         {
-            Task.Run(() => {
+            Task.Run(() =>
+            {
                 for (int i = 0; i < 8; i++)
                 {
                     chairStack.Push(new Chair());
@@ -94,6 +99,9 @@ namespace Labb_6
             //Bouncer events och prenumerationer
             bounce.GaDirektHem += Bouncer_GarDirektHem;
 
+            
+            //Skall vi gå en generisk eller specifik väg?????
+            
             //Guest events och prunumerationer
             //g.KommerInIPub += Guest_KommerInIPub;
             //g.LetarLedigStol += Guest_LetarEfterLedigStrol;
@@ -149,9 +157,11 @@ namespace Labb_6
             //Task_Bouncer 
             Task.Run(() =>
             {
+
+                bounce.InviteGuest(Guest_KommerInIPub, AddGuestToQueue);  //var tidigare i fel ordning!!  + mysko anrop
                 bounce.Bouncer_GaDirektHemMethod();
                 //Lägg in en till Action<Guest> CallGuest som param för kön.
-                bounce.InviteGuest(Guest_KommerInIPub, AddGuestToQueue);
+               
             });
 
 
@@ -171,7 +181,7 @@ namespace Labb_6
             //Task_Bartender 
             Task.Run(() =>
             {
-                b.Bartending(guestQueue, BartenderWork, glasstack, chairStack, BartenderPause);
+                b.Bartending(guestQueue, BartenderWork, glasstack, chairStack, BartenderPause, GuestActivities);  //mysko grejer igen
                 //b.VantaiBarMetod();
 
                 //b.PlockaGlasFranHyllaMetod();
@@ -206,9 +216,10 @@ namespace Labb_6
             }
         }
 
-        public void AddGuestToQueue(Guest guest)
+        public void AddGuestToQueue(Guest guest)  //tillhör gäst-klassen!!
         {
-            Task.Run(() => {
+            Task.Run(() =>
+            {
                 guestQueue.Enqueue(guest);
             });
         }
@@ -232,7 +243,11 @@ namespace Labb_6
             Dispatcher.Invoke(() => { lstbBartender.Items.Insert(0, timeStamp); });
         }
 
-        public void BartenderWork(string whatBartenderDoes)
+
+
+        //skall vi gå en generisk väg eller specifik väg??
+
+        public void BartenderWork(string whatBartenderDoes)  //whatBartenderDoes är därmed ett alldeles för icke-beskrivande namn eg men å andra sidan med en generisk modell så vad skall man göra eller kalla den?  MÅSTE välja vilken design vi skall ha!!!
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             Dispatcher.Invoke(() =>
@@ -240,6 +255,20 @@ namespace Labb_6
                 lstbBartender.Items.Insert(0, $"{timeStamp} " + whatBartenderDoes);
             });
         }
+
+        public void GuestActivities(string whatGuestDoes)  
+        {
+            String timeStamp = GetTimestamp(DateTime.Now);
+            Dispatcher.Invoke(() =>
+            {
+                lstbGaster.Items.Insert(0, $"{timeStamp} " + whatGuestDoes);
+            });
+        }
+
+
+
+        //skall vi inte använda de ustkrifts-metoder jag gjorde??????
+
         //public void Bartender_VantaIBaren()
         //{
         //    String timeStamp = GetTimestamp(DateTime.Now);
@@ -329,7 +358,7 @@ namespace Labb_6
             Dispatcher.Invoke(() => { lstbGaster.Items.Insert(0, timeStamp); });
         }
 
-        public void Guest_KommerInIPub(string call)
+        public void Guest_KommerInIPub(string call)  //call är också för icke-deskreptivt variabelnamn!
         {
             String timeStamp = GetTimestamp(DateTime.Now);
             timeStamp += call + " _Kommer in i puben";
@@ -406,25 +435,27 @@ namespace Labb_6
 
         private void btnBartender_Click(object sender, RoutedEventArgs e)
         {
-            BartenderPause = !BartenderPause;
+            BartenderPause = !BartenderPause;   //härmapa!! :-P  
         }
     } //mainwindow class ends here
 
 
     // OTHER CLASSES HERE:
-
+    
     public class Bouncer
     {
-        public Action<string> CallBack;
-        public Action<Guest> CallGuest;
+        public Action<string> CallBack;   //för generiskt nammn eg, men samma problematik som ovan. MÅSTE väjla design-modell!!
+
+        public Action<Guest> CallGuest;   //vad gör dessa lokala delegater eg?  används ju inte i koden nedan förutom tilldelningen????
+
         //Lägg in en till Action<Guest> CallGuest som param för kön.
         public void InviteGuest(Action<string> callback, Action<Guest> callGuest)
         {
             //Bouncer
             //Inkastaren släpper in kunder slumpvis, efter tre till tio sekunder. Inkastaren kontrollerar leg, så att alla i baren kan veta vad kunden heter. (Slumpa ett namn åt nya kunder från en lista) Inkastaren slutar släppa in nya kunder när baren stänger och går hem direkt.
 
-            CallBack = callback;
-            CallGuest = callGuest;
+            CallBack = callback;   //vad innebär denna tildelning egentligen??? EN AV DE SAKER JAG VERKLIGEN INTE FATTAR???
+            CallGuest = callGuest; 
 
             List<string> _listOfGuests = new List<string>
             {
@@ -459,8 +490,8 @@ namespace Labb_6
 
         public event Action GaDirektHem;
 
-        private int slappaInGastTid = 0;
-        public int SlappaInGastTid { get; set; } //(3-10s) 
+        //private int slappaInGastTid = 0;
+        public int SlappaInGastTid { get; set; } = 0; //(3-10s)   FLYTTAS TILL GUEST-KLASS!!  om det nu går som med alla flyttningar?
 
         public void Bouncer_GaDirektHemMethod()
         {
@@ -470,217 +501,236 @@ namespace Labb_6
 
 
     //Jari:  iom dessa klasser anropas från main via varsin respektive task så ingår ju redan de här metoderna i de trådarna som jag anser det.  de triggar ju vad som sedan via ovanstående utskriftsmetoder blir själva callback:et.  så om den tolkningen är rätt så blir det ju meninglösa att se de här metoderna i en task...när de redan är del av den iom anropen och deras triggning tillbaka till respektiva tråd i respektive task.  ällä???   låtar dem vara så länge iaf.   kanske itne ens går att dynga dit en task i varje klass här??
-}
 
 
-public class Guest
-{
-    public string Name { get; set; }
-    public Guest(string name)
+
+    public class Guest
     {
-        Name = name;
-    }
+        public string Name { get; set; }
 
-    public event Action KommerInIPub, LetarLedigStol, SatterSigNed, DrickaOlLamnaBar, BarenOppnas, BarenStangs;
-
-    private int antalGaster = 0;
-    public int AntalGaster { get; set; } //gäster-räknare. skall vara global?
-
-    private int kommaTillBarenTid = 0;
-    public int KommaTillBarenTid { get; set; } //(1s)
-
-    private int gaTillStolTid = 0;
-    public int GaTillStolTid { get; set; } //(4s)
-
-    private int drickaOlTid = 0;
-    public int DrickaOlTid { get; set; } //(10-20s) 
-
-    public void Guest_BarenOppnasMetod()
-    {
-        BarenOppnas?.Invoke();
-    }
-
-    public void Guest_BarenStangsMetod()
-    {
-        BarenStangs?.Invoke();
-    }
-
-    public void KommerInIPubMetod()
-    {
-        KommerInIPub?.Invoke();
-    }
-
-    public void LetarLedigStolMetod()
-    {
-        LetarLedigStol?.Invoke();
-    }
-
-    public void SatterSigNedMetod()
-    {
-        SatterSigNed?.Invoke();
-    }
-
-    public void DrickaOlLamnaBarMetod()
-    {
-        DrickaOlLamnaBar?.Invoke();
-    }
-}
-
-
-public class Bartender
-{
-    private Action<string> Callback;
-    private ConcurrentQueue<Guest> GuestQueue;
-    private ConcurrentStack<Glass> GlassStack;
-    private ConcurrentStack<Chair> ChairStack;
-
-
-    public void Bartending(ConcurrentQueue<Guest> guestQueue, Action<string> callback, ConcurrentStack<Glass> glassStack, ConcurrentStack<Chair> chairStack, bool barOpen)
-    {
-        Callback = callback;
-        GuestQueue = guestQueue;
-        GlassStack = glassStack;
-        ChairStack = chairStack;
-
-        Task.Run(() =>
+        public Guest(string name)  //vad används konstruktorn till? känns överflödig!! går ju instansiera objekt och tilldela namn till egenskapen ovan.
         {
-            while (barOpen) //Kommer att kolla om baren är öppen
+            Name = name;
+        }
+
+        public event Action KommerInIPub, LetarLedigStol, SatterSigNed, DrickaOlLamnaBar, BarenOppnas, BarenStangs;
+
+        private int antalGaster = 0;
+        public int AntalGaster { get; set; } //gäster-räknare. skall vara global?
+
+        private int kommaTillBarenTid = 0;
+        public int KommaTillBarenTid { get; set; } //(1s)
+
+        private int gaTillStolTid = 0;
+        public int GaTillStolTid { get; set; } //(4s)
+
+        private int drickaOlTid = 0;
+        public int DrickaOlTid { get; set; } //(10-20s) 
+
+        public void Guest_BarenOppnasMetod()
+        {
+            BarenOppnas?.Invoke();
+        }
+
+        public void Guest_BarenStangsMetod()
+        {
+            BarenStangs?.Invoke();
+        }
+
+        public void KommerInIPubMetod()
+        {
+            KommerInIPub?.Invoke();
+        }
+
+        public void LetarLedigStolMetod()
+        {
+            LetarLedigStol?.Invoke();
+        }
+
+        public void SatterSigNedMetod()
+        {
+            SatterSigNed?.Invoke();
+        }
+
+        public void DrickaOlLamnaBarMetod()
+        {
+            DrickaOlLamnaBar?.Invoke();
+        }
+    }
+
+
+    public class Bartender
+    {
+        private Action<string> Callback;
+        public Action<String> CallGuest;  //för utskrift i gäst-listboxen!!!  se även "dynamisk" metod jag gjorde ovan för det!!
+        private ConcurrentQueue<Guest> GuestQueue;   //ÅTER IGEN FATTAR ABSOLUT INTE SYFTET MED DE ÄR LOKALA DELEGATERNA, COLLECTIONS ETC SOM INTE ANVÄNDS MER ÄN VID TILLDELNINGN NEDAN????
+        private ConcurrentStack<Glass> GlassStack;
+        private ConcurrentStack<Chair> ChairStack;
+
+
+        public void Bartending(ConcurrentQueue<Guest> guestQueue, Action<string> callback,
+            ConcurrentStack<Glass> glassStack, ConcurrentStack<Chair> chairStack, bool barOpen, Action<String> callGuest)
+        {
+            Callback = callback;          //SAMMA DÅ HÄR, VAD ÄR VITSEN???
+            CallGuest = callGuest;
+            GuestQueue = guestQueue;
+            GlassStack = glassStack;
+            ChairStack = chairStack;
+
+            Task.Run(() =>
             {
-
-                Thread.Sleep(1000);
-
-                if (!guestQueue.IsEmpty)
+                while (barOpen) //Kommer att kolla om baren är öppen
                 {
-                    if (!glassStack.IsEmpty)
 
+                    //korrigerat kod nedan, ändrat på ordning som var fel och främst lagt till sleep-loopar som verkar funka!!
 
+                     
+
+                    if (!guestQueue.IsEmpty)
                     {
-                        callback("Bartendern tar ett glas från glashyllan.");
-                        Thread.Sleep(3000);
-                        GlassStack.TryPop(out Glass glass);
-                        if (!chairStack.IsEmpty)
+                        Thread.Sleep(1000);  //tar en sekund för en ev gäst att nå baren. om ingen gäst, ingen fördröjning!
+
+                        if (!glassStack.IsEmpty)
                         {
-                            ChairStack.TryPop(out Chair chair);
+                            callback("Bartendern tar ett glas från glashyllan.");
+                            Thread.Sleep(3000);
+                            GlassStack.TryPop(out Glass glass);     //FATTAR INTE KONSTRUKTONEN MED OUT. FÖRKLARING TACK!!
+                            
                             callback($"Bartendern häller upp en öl till {guestQueue.First().Name}.");
                             Thread.Sleep(3000);
+                            
+                            if (!chairStack.IsEmpty)   //bartendern häller upp öl först...SEDAN går gästen till en stol etc!! fel ordnig förut!!
+                            {
+                                callGuest($"{guestQueue.First().Name} går till en ledig stol");  //skrivs ut i gäst-listboxen!
+                                Thread.Sleep(4000);  //gästen går till stolen, 4sek, saknades förut!!
+                                ChairStack.TryPop(out Chair chair);       
+
+                                
+                            }
+                            else
+                            {
+                                callGuest($"{guestQueue.First().Name}. väntar på en stol");  ////skrivs ut i gäst-listboxen!
+                                while (chairStack.IsEmpty)
+                                    Thread.Sleep(50);
+                            }
+                            guestQueue.TryDequeue(out Guest guest);
                         }
+
+
                         else
                         {
-                            callback($"{guestQueue.First().Name}. väntar på en stol");
+                            callback("Bartendern väntar på glas");
+                            while (glassStack.IsEmpty)
+                                Thread.Sleep(50);
                         }
-                        guestQueue.TryDequeue(out Guest guest);
+
+
+
+
                     }
-
-
                     else
                     {
-                        callback("Bartendern väntar på glass");
+                        callback("Bartendern löser sudoku medan han väntar på kunder.");
+                        while(guestQueue.IsEmpty)
+                        Thread.Sleep(50);
                     }
-
-
-
-
                 }
-                else
-                {
-                    callback("Bartendern löser sudoku medan han väntar på kunder.");
-                }
-            }
-        });
+            });
+        }
+
+        public event Action VantaiBar, PlockaGlasFranHylla, HallaUppOl, BartenderGarHem, BarenOppnas, BarenStangs;
+
+        private int antalGlas = 0;
+        public int AntalGlas { get; set; } //skall vara global??
+
+        private int hamtaGlasTid = 0;
+        public int HamtaGlasTid { get; set; } //(3s)
+
+        private int hallaOlTid = 0;
+        public int HallaOlTid { get; set; } //(3s)
+
+        public void Bartender_BarenOppnasMetod()
+        {
+            BarenOppnas?.Invoke();
+        }
+
+        public void Bartender_BarenStangsMetod()
+        {
+            BarenStangs?.Invoke();
+        }
+
+        public void VantaiBarMetod()
+        {
+            VantaiBar?.Invoke();
+        }
+
+        public void PlockaGlasFranHyllaMetod()
+        {
+            PlockaGlasFranHylla?.Invoke();
+        }
+
+        public void HallaUppOlMetod()
+        {
+            HallaUppOl?.Invoke();
+        }
+
+        public void BartenderGarHemMetod()
+        {
+            BartenderGarHem?.Invoke();
+        }
+
+
+        //ingen task, metod som anropar häll upp öl osv. task i main dock!! (dave)  SKALL VI BAARA EN LR NÅGRA TASKS I MIAN DÅ OCH FRÅN DEN/DEMStrANROPA METODER                                                                           I KLASSER ETC???  JAG HAR DOCK GJORT  LITE TRÅDAR I VARJE KLASS. FÅR VÄL                                                                              ÄNDRA PÅ DET SEDAN ISF??
     }
-    public event Action VantaiBar, PlockaGlasFranHylla, HallaUppOl, BartenderGarHem, BarenOppnas, BarenStangs;
 
-    private int antalGlas = 0;
-    public int AntalGlas { get; set; } //skall vara global??
-
-    private int hamtaGlasTid = 0;
-    public int HamtaGlasTid { get; set; } //(3s)
-
-    private int hallaOlTid = 0;
-    public int HallaOlTid { get; set; } //(3s)
-
-    public void Bartender_BarenOppnasMetod()
+    public class Glass
     {
-        BarenOppnas?.Invoke();
+
     }
 
-    public void Bartender_BarenStangsMetod()
+    public class Chair
     {
-        BarenStangs?.Invoke();
+
     }
 
-    public void VantaiBarMetod()
+    public class Waiter
     {
-        VantaiBar?.Invoke();
+        public event Action PlockarTommaGlas, DiskarGlas, StallarGlasIHylla, BarenOppnas, BarenStangs;
+
+        private int antalStolar = 0;
+        public int AntalStolar { get; set; } //skall vara global??
+
+        private int plockaTommaGlasTid = 0;
+        public int PlockaTommaGlasTid { get; set; } //(10s)  
+
+        private int diskaGlasTid = 0;
+        public int DiskaGlasTid { get; set; } //(15s)
+
+        public void Waiter_BarenOppnasMetod()
+        {
+            BarenOppnas?.Invoke();
+        }
+
+        public void Waiter_BarenStangsMetod()
+        {
+            BarenStangs?.Invoke();
+        }
+
+        public void PlockarTommaGlasMetod()
+        {
+            PlockarTommaGlas?.Invoke();
+        }
+
+        public void DiskarGlasMetod()
+        {
+            DiskarGlas?.Invoke();
+        }
+
+        public void StallarGlasIHyllaMetod()
+        {
+            StallarGlasIHylla?.Invoke();
+        }
     }
 
-    public void PlockaGlasFranHyllaMetod()
-    {
-        PlockaGlasFranHylla?.Invoke();
-    }
 
-    public void HallaUppOlMetod()
-    {
-        HallaUppOl?.Invoke();
-    }
-
-    public void BartenderGarHemMetod()
-    {
-        BartenderGarHem?.Invoke();
-    }
-
-
-    //ingen task, metod som anropar häll upp öl osv. task i main dock!! (dave)  SKALL VI BAARA EN LR NÅGRA TASKS I MIAN DÅ OCH FRÅN DEN/DEMStrANROPA METODER                                                                           I KLASSER ETC???  JAG HAR DOCK GJORT  LITE TRÅDAR I VARJE KLASS. FÅR VÄL                                                                              ÄNDRA PÅ DET SEDAN ISF??
-}
-
-public class Glass
-{
-
-}
-
-public class Chair
-{
-
-}
-
-public class Waiter
-{
-    public event Action PlockarTommaGlas, DiskarGlas, StallarGlasIHylla, BarenOppnas, BarenStangs;
-
-    private int antalStolar = 0;
-    public int AntalStolar { get; set; } //skall vara global??
-
-    private int plockaTommaGlasTid = 0;
-    public int PlockaTommaGlasTid { get; set; } //(10s)  
-
-    private int diskaGlasTid = 0;
-    public int DiskaGlasTid { get; set; } //(15s)
-
-    public void Waiter_BarenOppnasMetod()
-    {
-        BarenOppnas?.Invoke();
-    }
-
-    public void Waiter_BarenStangsMetod()
-    {
-        BarenStangs?.Invoke();
-    }
-
-    public void PlockarTommaGlasMetod()
-    {
-        PlockarTommaGlas?.Invoke();
-    }
-
-    public void DiskarGlasMetod()
-    {
-        DiskarGlas?.Invoke();
-    }
-
-    public void StallarGlasIHyllaMetod()
-    {
-        StallarGlasIHylla?.Invoke();
-    }
-}
-
-
-//EOF
+} //EOF   namespacet skall sluta HÄR, SÅ PILLA INTE!!!
